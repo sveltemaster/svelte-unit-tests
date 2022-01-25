5789d0639327365009453c9b7fe5a7674ae94d22
Amadeus J <amadeus@solofunds.com>
2022-01-25 15:53:15 -0500

    try importing from material UI

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,8 @@
 <script>
+	import Button from "@smui/button";
 	let message = 'hi';
 </script>
+<Button></Button>
 <div>{message}</div>
 <input id="message" type="text" bind:value={message}><br>
 <button on:click={() => message='bye'}>say bye</button>
